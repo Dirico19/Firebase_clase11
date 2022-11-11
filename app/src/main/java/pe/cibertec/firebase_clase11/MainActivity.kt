c7aa2c7226166ea97b280a28660ee35d4ad39162
Dirico19 <diego090819@gmail.com>
2022-11-11 12:02:15 -0500

    Initial commit

---
+++ app/src/main/java/pe/cibertec/firebase_clase11/MainActivity.kt
@@ -0,0 +1,44 @@
+package pe.cibertec.firebase_clase11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val db = Firebase.firestore
+
+        val collectionName = "Personas"
+
+        // LEER
+        db.collection(collectionName)
+            .get()
+            .addOnSuccessListener { query ->
+                for (doc in query) {
+                    Log.i("DATA","Documento con ID: ${doc.id} => ${doc.data}")
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.e("ERROR","No se pudo leer $e")
+            }
+/*
+        // INSERTAR
+        val persona = hashMapOf(
+            "nombre" to "Ramiro",
+            "apellido" to "Gomez",
+            "edad" to 30
+        )
+
+        db.collection(collectionName).add(persona).addOnSuccessListener { docref ->
+            Log.i("EXITO","Se registro la nueva persona con id ${docref.id}")
+        }.addOnFailureListener { e ->
+            Log.e("ERROR","No se pudo registrar / $e")
+        }
+*/
+    }
+}
